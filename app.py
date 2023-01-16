189b5d36ff5275299005cd814bf56fd481d79b0d
Adrien Gasnault <adpro@macbook-pro-de-adrien-1.home>
2023-01-16 16:55:27 +0100

    modif api

--- app.py
+++ app.py
@@ -47,3 +47,9 @@ def prep_p(data:PreClass):
         'format' : prob_pred_format,
         'probabilité': proba_value_1
     }
+    
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+#uvicorn main:app --reload
